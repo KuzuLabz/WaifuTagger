7a277bbd74a80b43b16a5f0b9c0aa719aa59e5be
Smashinfries <jus10luka@gmail.com>
2024-03-06 22:04:01 -0600

    update dependencies

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { ActivityIndicator, Button, Linking, StyleSheet, Text, View } from 'react-native';
-import { InferenceSession, Tensor } from 'onnxruntime-react-native';
 import * as DocumentPicker from 'expo-document-picker';
-import { useState } from 'react';
-import * as ImagePicker from 'expo-image-picker';
 import * as FileSystem from 'expo-file-system';
+import * as ImagePicker from 'expo-image-picker';
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { ActivityIndicator, Button, Linking, StyleSheet, Text, View } from 'react-native';
+import { useTensorflowModel, Tensor } from 'react-native-fast-tflite';
 
 // load a model
 // const session: InferenceSession = await InferenceSession.create(modelPath);
@@ -31,10 +31,8 @@ const getImageBuffer = async (uri: string): Promise<Float32Array | undefined> =>
 
 export default function App() {
     const [image_uri, setImageUri] = useState<ImagePicker.ImagePickerAsset | null>(null);
-    const [session, setSession] = useState<InferenceSession | null>(null);
-    const [modelUri, setModelUri] = useState<string | null>(null);
-
-    const [isLoadingModel, setIsLoadingModel] = useState(false);
+    // const [isLoadingModel, setIsLoadingModel] = useState(false);
+    const { model, state } = useTensorflowModel(require('./assets/model_float16.tflite'));
 
     const pickImage = async () => {
         try {
@@ -49,68 +47,36 @@ export default function App() {
         }
     };
 
-    const unloadModel = async () => {
-        if (session) {
-            await session.release();
-            console.log('released!');
-        }
-    };
 
-    const runInference = async (session: InferenceSession) => {
-        if (session && image_uri) {
-            const input_name = session.inputNames[0];
-            const label_name = session.outputNames[0];
-            console.log('Input:', input_name);
-            console.log('Output:', label_name);
-            // const input: InferenceSession.OnnxValueMapType = {
-            //     [input_name]: new Tensor('float32', [1, 3, 224, 224]),
-            // };
-
-            // const preds = await session.run(input);
-        }
-    };
-
-    const loadModel = async (uri: string) => {
-        if (modelUri) {
-            setIsLoadingModel(true);
-            try {
-                const session = await InferenceSession.create(uri);
-                setSession(session);
-            } catch (error) {
-                console.error(error);
-            }
-            setIsLoadingModel(false);
-        }
-    };
-
-    const pickModel = async () => {
-        const model = await DocumentPicker.getDocumentAsync({ multiple: false });
-        if (model.assets) {
-            setModelUri(model.assets[0].uri);
-            loadModel(model.assets[0].uri);
-        } else {
-            return null;
+    const runInference = async () => {
+        if (state === 'loaded') {
+            // model.inputs.forEach(())
+            console.log(model.inputs);
+            // const input = await getImageBuffer(image_uri?.uri);
+            // if (input) {
+            //     const tensor = new Tensor('float32', input, [1, 224, 224, 3]);
+            //     const result = await session.run([tensor]);
+            //     console.log(result);
+            // }
         }
     };
 
     return (
         <View style={styles.container}>
-            <Button
+            {/* <Button
                 title="Download Model"
                 onPress={() =>
                     Linking.openURL(
                         'https://huggingface.co/SmilingWolf/wd-convnext-tagger-v3/tree/main',
                     )
                 }
-            />
-            {/* <Button title="Pick Model" onPress={pickModel} />
-            {isLoadingModel ? <ActivityIndicator /> : null}
+            /> */}
 
-            <Text>{session ? 'Model is Loaded!' : 'Model not loaded!'}</Text>
+            <Text>State: {state}</Text>
 
             <Button title="Pick an image" onPress={pickImage} />
 
-            <Button title="Unload Model" onPress={unloadModel} /> */}
+            <Button title="Run Inference" onPress={runInference} />
 
             <StatusBar style="auto" />
         </View>
