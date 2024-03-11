29e1e0e6a772e8248db31188134d469adcb35dad
Smashinfries <jus10luka@gmail.com>
2024-03-11 18:45:16 -0500

    Add Toaster component to App.tsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { MD3DarkTheme, PaperProvider } from 'react-native-paper';
 import Main from './src';
+import { Toaster } from 'burnt/web';
 
 const App = () => {
     return (
         <PaperProvider theme={MD3DarkTheme}>
             <Main />
             <StatusBar style="light" />
+            <Toaster position="bottom-right" />
         </PaperProvider>
     );
 };
