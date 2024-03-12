690c4ab82c15317d91ed831c722bc38876b5af59
Smashinfries <jus10luka@gmail.com>
2024-03-12 12:45:35 -0500

    Add ImageChangeEvent and AppSettings components

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { Image } from 'expo-image';
-import { ScrollView, StyleSheet, View } from 'react-native';
+import { ImageChangeEvent, ScrollView, StyleSheet, View } from 'react-native';
 import useModel from './hooks/useModel';
 import {
 	Appbar,
@@ -18,34 +18,36 @@ import LoadingView from './components/loading';
 import ImageSelector from './components/imageSelector';
 import ResultSection from './components/section';
 import TagText from './components/tagText';
-import { AppInfo, InferenceConfigurator, TagInfo } from './components/dialogs';
-import { useState } from 'react';
+import { AppInfo, AppSettings, InferenceConfigurator, TagInfo } from './components/dialogs';
+import { useEffect, useState } from 'react';
 import { InferenceTag } from './types';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Footer from './components/footer';
 import Constants from 'expo-constants';
+import { useSettingsStore } from './store';
 
-const Main = () => {
+const Main = ({ updateTheme }: { updateTheme: (sourceColor: string) => void }) => {
+	const { colorMode } = useSettingsStore();
+
 	const [tagInfoVisible, setTagInfoVisible] = useState(false);
 	const [configVisible, setConfigVisible] = useState(false);
 	const [appInfoVisible, setAppInfoVisible] = useState(false);
+	const [appSettingsVisible, setAppSettingsVisible] = useState(false);
 
 	const [url, setUrl] = useState('');
-
-	const { top } = useSafeAreaInsets();
 
 	const [selectedTag, setSelectedTag] = useState<InferenceTag | null>(null);
 	const {
 		runInference,
 		pickImage,
-		updateConfig,
 		takePicture,
 		loadFromUrl,
+		onImagePaste,
+		imageColors,
 		tags,
 		image,
 		loading,
 		isInferLoading,
-		inferenceConfig,
 	} = useModel();
 
 	const onTagSelect = (tag: InferenceTag) => {
@@ -53,43 +55,47 @@ const Main = () => {
 		setTagInfoVisible(true);
 	};
 
+	useEffect(() => {
+		if (imageColors) {
+			if (imageColors.platform === 'android') {
+				updateTheme(imageColors[colorMode]);
+			}
+		}
+	}, [imageColors]);
+
 	return loading ? (
 		<LoadingView />
 	) : (
 		<View style={{ height: '100%' }}>
 			<Appbar.Header mode="center-aligned">
-				<Appbar.Content title={Constants.expoConfig?.name} />
 				<Appbar.Action
 					icon={'information-outline'}
 					onPress={() => setAppInfoVisible(true)}
 				/>
+				<Appbar.Content title={Constants.expoConfig?.name} />
+				<Appbar.Action icon={'cog-outline'} onPress={() => setAppSettingsVisible(true)} />
 			</Appbar.Header>
 			<ScrollView contentContainerStyle={{ flexGrow: 1 }} keyboardDismissMode="on-drag">
 				<View style={{ height: '100%' }}>
-					{/* <View
-						style={{
-							flexDirection: 'row',
-							justifyContent: 'space-between',
-							paddingTop: top + 10,
-						}}
-					>
-						<Text>{Constants.expoConfig?.name}</Text>
-						<IconButton
-							icon="information-outline"
-							onPress={() => setAppInfoVisible(true)}
-						/>
-					</View> */}
 					<ImageSelector
 						onImagePick={pickImage}
 						image={image}
 						isLoading={isInferLoading}
 					/>
-					<View style={{ flexDirection: 'row', marginTop: 15, width: '100%' }}>
+					<View
+						style={{
+							flexDirection: 'row',
+							marginTop: 15,
+							width: '100%',
+							alignItems: 'center',
+						}}
+					>
 						<TextInput
 							mode="outlined"
 							label={'Image URL'}
 							value={url}
 							onChangeText={(text) => setUrl(text)}
+							onImageChange={onImagePaste}
 							style={{ marginLeft: 10, flex: 1 }}
 							autoFocus={false}
 							right={
@@ -151,12 +157,14 @@ const Main = () => {
 					tag={selectedTag}
 				/>
 				<InferenceConfigurator
-					defaultConfig={inferenceConfig}
-					onChange={(cf) => updateConfig(cf)}
 					visible={configVisible}
 					onDismiss={() => setConfigVisible(false)}
 				/>
 				<AppInfo visible={appInfoVisible} onDismiss={() => setAppInfoVisible(false)} />
+				<AppSettings
+					visible={appSettingsVisible}
+					onDismiss={() => setAppSettingsVisible(false)}
+				/>
 			</Portal>
 		</View>
 	);
