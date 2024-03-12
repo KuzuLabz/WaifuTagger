4dfbe196396c59a0a988e2377e01853b96584ff9
Smashinfries <jus10luka@gmail.com>
2024-03-12 12:45:13 -0500

    Add theme prop to Text component in LoadingView

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,12 +1,14 @@
 import { View } from 'react-native';
 import { ActivityIndicator, Text } from 'react-native-paper';
 import Constants from 'expo-constants';
+import { useAppTheme } from '../theme';
 
 const LoadingView = () => {
+	const theme = useAppTheme();
 	return (
 		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
 			<ActivityIndicator size="large" />
-			<Text>Booting up {Constants.expoConfig?.name}</Text>
+			<Text theme={theme}>Booting up {Constants.expoConfig?.name}</Text>
 		</View>
 	);
 };
