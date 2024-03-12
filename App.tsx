5d85860b6e1f5fe5b6d3af9bbde85e622e3bda1f
Smashinfries <jus10luka@gmail.com>
2024-03-11 19:48:14 -0500

    Add color scheme support and useMaterial3Theme hook

--- App.tsx
+++ App.tsx
@@ -1,16 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { MD3DarkTheme, PaperProvider } from 'react-native-paper';
+import { MD3DarkTheme, MD3LightTheme, PaperProvider } from 'react-native-paper';
 import Main from './src';
 import { Toaster } from 'burnt/web';
+import { useColorScheme } from 'react-native';
+import { useMaterial3Theme } from '@pchmn/expo-material3-theme';
+import { useEffect, useMemo } from 'react';
 
 const App = () => {
-    return (
-        <PaperProvider theme={MD3DarkTheme}>
-            <Main />
-            <StatusBar style="light" />
-            <Toaster position="bottom-right" />
-        </PaperProvider>
-    );
+	const colorScheme = useColorScheme();
+	// If the device is not compatible, it will return a theme based on the fallback source color (optional, default to #6750A4)
+	const { theme } = useMaterial3Theme({ fallbackSourceColor: '#3E8260' });
+
+	const paperTheme = useMemo(
+		() =>
+			colorScheme === 'dark'
+				? { ...MD3DarkTheme, colors: theme.dark }
+				: { ...MD3LightTheme, colors: theme.light },
+		[colorScheme, theme],
+	);
+
+	useEffect(() => {
+		console.log(colorScheme);
+	}, [colorScheme]);
+
+	return (
+		<PaperProvider theme={paperTheme}>
+			<Main />
+			<StatusBar style="light" />
+			<Toaster position="bottom-right" />
+		</PaperProvider>
+	);
 };
 
 export default App;
