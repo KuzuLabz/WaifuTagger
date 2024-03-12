459999ac0ea54ffc5ee3646678070d76c24ba719
Smashinfries <jus10luka@gmail.com>
2024-03-12 12:44:29 -0500

    Add openBrowser function to utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import * as Clipboard from 'expo-clipboard';
+import * as WebBrowser from 'expo-web-browser';
 
 export const zip = (arrays: any[]) => {
 	return arrays[0].map(function (_: any, i: string | number) {
@@ -12,25 +13,6 @@ export const copyToClipboard = async (text: string | number) => {
 	await Clipboard.setStringAsync(`${text}`);
 };
 
-export const processBooruTagBody = (body: string) => {
-	const newBody = body
-		.replaceAll('[i]', '')
-		.replaceAll('[/i]', '')
-		.replaceAll('[b]', '')
-		.replaceAll('[/b]', '')
-		.split('h4.')[0];
-	const doubleBracket = newBody.match(/\[\[.*?\]\]/g);
-	if (doubleBracket) {
-		const splitBody = newBody.split(/[[\]]{1,2}/g);
-		for (let i = 0; i < splitBody.length; i++) {
-			if (splitBody[i].includes('|')) {
-				const splitBracket = splitBody[i].split('|');
-				splitBody[i] = splitBracket.at(-1);
-			}
-		}
-		const removedBrackets = splitBody.join('');
-		return removedBrackets;
-	} else {
-		return newBody;
-	}
+export const openBrowser = async (url: string, options?: WebBrowser.WebBrowserOpenOptions) => {
+	await WebBrowser.openBrowserAsync(url, options);
 };
