e1025d7e8da3d97c6bfbfffdbac01685bd2ded80
Smashinfries <jus10luka@gmail.com>
2024-03-12 15:18:01 -0500

    Update metro.config.js to use ES6 import syntax and add support for .onnx files

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,10 @@
-const { getDefaultConfig } = require('expo/metro-config');
+import { getDefaultConfig } from 'expo/metro-config';
 
 const config = getDefaultConfig(__dirname);
 
 config.resolver.assetExts.push(
-    // Adds support for `.db` files for SQLite databases
-    'onnx',
+	// Adds support for `.db` files for SQLite databases
+	'onnx',
 );
 
-module.exports = config;
+export default config;
