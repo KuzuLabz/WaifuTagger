ea7794221905aafaccc1e6548a6ac0a988b14e8b
Smashinfries <jus10luka@gmail.com>
2024-03-13 02:50:19 -0500

    Add toast for image error

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
 import * as Clipboard from 'expo-clipboard';
 import * as WebBrowser from 'expo-web-browser';
+import Burnt from 'burnt';
 
 export const zip = (arrays: any[]) => {
 	return arrays[0].map(function (_: any, i: string | number) {
@@ -16,3 +17,10 @@ export const copyToClipboard = async (text: string | number) => {
 export const openBrowser = async (url: string, options?: WebBrowser.WebBrowserOpenOptions) => {
 	await WebBrowser.openBrowserAsync(url, options);
 };
+
+export const toastImageError = () =>
+	Burnt.toast({
+		title: 'Image must be PNG or JPEG',
+		haptic: 'error',
+		preset: 'error',
+	});
