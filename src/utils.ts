865779764384627bc8b4a0441a451fbdd9d9ddfb
Smashinfries <jus10luka@gmail.com>
2024-03-14 18:54:17 -0500

    Fix import statement in utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 import * as Clipboard from 'expo-clipboard';
 import * as WebBrowser from 'expo-web-browser';
-import Burnt from 'burnt';
+import * as Burnt from 'burnt';
 
 export const zip = (arrays: any[]) => {
 	return arrays[0].map(function (_: any, i: string | number) {
@@ -18,7 +18,7 @@ export const openBrowser = async (url: string, options?: WebBrowser.WebBrowserOp
 	await WebBrowser.openBrowserAsync(url, options);
 };
 
-export const toastImageError = () =>
+export const toastImageError = (): void =>
 	Burnt.toast({
 		title: 'Image must be PNG or JPEG',
 		haptic: 'error',
