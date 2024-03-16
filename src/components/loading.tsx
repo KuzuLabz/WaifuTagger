f837b430255490be228ba0f397abb654990f6261
Smashinfries <jus10luka@gmail.com>
2024-03-16 00:07:09 -0500

    use surface instead of view

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,15 +1,22 @@
 import { View } from 'react-native';
-import { ActivityIndicator, Text } from 'react-native-paper';
+import { ActivityIndicator, Surface, Text } from 'react-native-paper';
 import Constants from 'expo-constants';
 import { useAppTheme } from '../theme';
 
 const LoadingView = () => {
 	const theme = useAppTheme();
 	return (
-		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+		<Surface
+			style={{
+				flex: 1,
+				justifyContent: 'center',
+				alignItems: 'center',
+				// backgroundColor: theme.colors.background,
+			}}
+		>
 			<ActivityIndicator size="large" />
 			<Text theme={theme}>Booting up {Constants.expoConfig?.name}</Text>
-		</View>
+		</Surface>
 	);
 };
 
