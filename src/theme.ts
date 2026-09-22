a4f832c853351eacbd75180877b712b4539c72e4
Smashinfries <jus10luka@gmail.com>
2026-09-22 15:52:23 -0500

    feat!: remove old files

--- src/theme.ts
+++
@@ -1,4 +0,0 @@
-import { Material3Scheme } from '@pchmn/expo-material3-theme';
-import { MD3Theme, useTheme } from 'react-native-paper';
-
-export const useAppTheme = useTheme<MD3Theme & { colors: Material3Scheme }>;
