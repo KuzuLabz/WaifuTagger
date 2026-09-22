9ad170aaa865b7dce623e44a0aa5d25740f90354
Smashinfries <jus10luka@gmail.com>
2026-09-22 16:15:05 -0500

    feat: refactor LoadingView to use CircularProgress and improve theme integration

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,25 +1,33 @@
-import { ActivityIndicator, Surface, Text } from 'react-native-paper';
-import { useAppTheme } from '../theme';
+import { Text } from 'react-native-paper';
 import { useLingui } from '@lingui/react/macro';
+import { useAppTheme } from '../providers/theme';
+import { Platform, View } from 'react-native';
+import { CircularProgress } from './common/progressBar';
+import { useModelsStore } from '../store/models';
+import { MODEL_CATALOG } from '../constants';
 
 const LoadingView = () => {
-	const theme = useAppTheme();
+    const { colors } = useAppTheme();
+    const selected = useModelsStore(state => state.selected);
     const { t } = useLingui();
+
+    if (!selected) {
+        return null;
+    }
     
 	return (
-		<Surface
+		<View
 			style={{
 				flex: 1,
 				justifyContent: 'center',
 				alignItems: 'center',
 				gap: 12,
+                backgroundColor: colors.surface
 			}}
 		>
-			<ActivityIndicator size="large" />
-			<Text theme={theme} selectable={false}>
-				{t`Loading model...`}
-			</Text>
-		</Surface>
+			<CircularProgress hosted={Platform.OS !== 'web'} value={0} indeterminate matchContents />
+            <Text>{t`Loading ${MODEL_CATALOG[selected.type].name}`}</Text>
+		</View>
 	);
 };
 
