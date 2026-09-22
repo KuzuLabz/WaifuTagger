46e1661b92f374d1f1d0d02829e7a8a9bac26abc
Smashinfries <jus10luka@gmail.com>
2026-09-22 16:22:54 -0500

    feat: refactor ResultSection to improve tag handling and integrate category translations

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -2,46 +2,46 @@ import { Pressable, View } from 'react-native';
 import { InferenceTag, InferenceTags } from '../types';
 import { Text, useTheme } from 'react-native-paper';
 import Result from './result';
-import { copyToClipboard } from '../utils';
+import { copyToClipboard } from '../utils/utils';
 import { useLingui } from '@lingui/react/macro';
+import { ModelSettings } from '../store/models';
+import { useStatsStore } from '../store/stats';
+import { useCategoryTitles } from '../hooks/translations/useCategoryTitles';
 
-type TitleType = 'Character' | 'Ratings' | 'General';
 type RatingsProps = {
-	title: TitleType;
+	title: keyof ModelSettings['thresholds'];
 	tags?: InferenceTags['rating'];
 	onTagSelect: (tag: InferenceTag) => void;
 };
+
+const nonBooruTags:(keyof ModelSettings['thresholds'])[] = ['year', 'rating']
+
 const ResultSection = ({ tags, title, onTagSelect }: RatingsProps) => {
 	const { colors } = useTheme();
     const { t } = useLingui();
-
-    const titleTranslations: Record<TitleType, string> = {
-        Character: t`Character`,
-        Ratings: t`Ratings`,
-        General: t`General`,
-    };
+    const categoryTrans = useCategoryTitles();
+    const isRankEnabled = useStatsStore((state) => state.isEnabled);
 
 	if (!tags) return;
+
 	return (
 		<View style={{ marginVertical: 10 }}>
 			<View style={{ marginLeft: 10, flexDirection: 'row', alignItems: 'center' }}>
-				<Text variant="headlineSmall">{titleTranslations[title]}</Text>
-				{title === 'General' && (
-					<View
-						style={{
-							backgroundColor: colors.surfaceVariant,
-							padding: 2,
-							paddingHorizontal: 5,
-							borderRadius: 6,
-							maxHeight: 24,
-							marginLeft: 5,
-						}}
-					>
-						<Text variant="labelSmall" style={{ color: colors.onSurfaceVariant }}>
-							{tags.length}
-						</Text>
-					</View>
-				)}
+				<Text variant="headlineSmall">{categoryTrans[title]}</Text>
+				<View
+                    style={{
+                        backgroundColor: colors.surfaceVariant,
+                        padding: 2,
+                        paddingHorizontal: 5,
+                        borderRadius: 6,
+                        maxHeight: 24,
+                        marginLeft: 5,
+                    }}
+                >
+                    <Text variant="labelSmall" style={{ color: colors.onSurfaceVariant }}>
+                        {tags.length}
+                    </Text>
+                </View>
 			</View>
 			<View style={{ alignSelf: 'center', width: '95%', padding: 10 }}>
 				{tags.length > 0 ? (
@@ -49,12 +49,12 @@ const ResultSection = ({ tags, title, onTagSelect }: RatingsProps) => {
 						<Pressable
 							key={idx}
 							onPress={() => {
-								tag.category !== 9 && onTagSelect(tag);
+								!nonBooruTags.includes(title) && onTagSelect(tag);
 							}}
-							onLongPress={() => copyToClipboard(tag.name)}
-							disabled={title === 'Ratings'}
+							onLongPress={() => copyToClipboard(tag.label)}
+							disabled={title === 'rating'}
 						>
-							<Result {...tag} />
+							<Result {...tag} rank={isRankEnabled ? tag.rank : undefined} />
 						</Pressable>
 					))
 				) : (
