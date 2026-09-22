b763cd9abc9c6865043f7f0eb16e24c66aebca31
Smashinfries <jus10luka@gmail.com>
2026-09-22 16:25:11 -0500

    feat: refactor LevelView component for improved styling and localization support

--- src/components/levelView.tsx
+++ src/components/levelView.tsx
@@ -1,58 +1,67 @@
-import { Platform, StyleSheet, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { useStatsStore } from '../store/stats';
-import { ProgressBar, Text } from 'react-native-paper';
+import { Text, ProgressBar } from 'react-native-paper';
+import { useLingui } from '@lingui/react/macro';
+import Color from 'color';
+import { useAppTheme } from '../providers/theme';
 
 export const LevelView = ({ isLoading }: { isLoading: boolean }) => {
-	const { isEnabled, levelInfo } = useStatsStore();
+    const isEnabled = useStatsStore(state => state.isEnabled);
+    const levelInfo = useStatsStore(state => state.levelInfo);
+    const { t } = useLingui();
+    const { colors } = useAppTheme();
 
-	const style = StyleSheet.create({
-		text: {
-			color: '#FFF',
-			fontWeight: '900',
-			textShadowRadius: 10,
-			textShadowOffset: { height: 2, width: 2 },
-			textShadowColor: '#000',
-		},
-	});
+    const style = StyleSheet.create({
+        text: {
+            color: colors.onSecondaryContainer,
+            fontWeight: '900',
+            fontSize: 12
+        },
+    });
 
-	if (!isEnabled) {
-		return null;
-	}
+    if (!isEnabled) {
+        return null;
+    }
 
-	return (
-		<View style={{ overflow: 'hidden' }}>
-			<ProgressBar
-				progress={levelInfo.xp / levelInfo.levelXpCap}
-				indeterminate={isLoading}
-				style={{ width: '100%', borderRadius: 0, height: Platform.OS === 'web' ? 12 : 18 }}
-			/>
-			<View
-				style={{
-					position: 'absolute',
-					height: '100%',
-					width: '100%',
-					justifyContent: 'center',
-					alignItems: 'center',
-					overflow: 'visible',
-				}}
-			>
-				<View
-					style={{
-						position: 'absolute',
-						left: 0,
-						height: '100%',
-						justifyContent: 'center',
-						paddingHorizontal: 6,
-					}}
-				>
-					<Text selectable={false} style={[style.text]}>
-						Lvl. {levelInfo.level}
-					</Text>
-				</View>
-				<Text selectable={false} style={[style.text]}>
-					{levelInfo.xp} / {levelInfo.levelXpCap}
-				</Text>
-			</View>
-		</View>
-	);
+    return (
+
+        <View style={{ overflow: 'hidden', justifyContent: 'center', height: 20 }}>
+            <ProgressBar
+                progress={levelInfo.xp / levelInfo.levelXpCap}
+                indeterminate={isLoading}
+                style={{ width: '100%', borderRadius: 0, height: '100%' }}
+            />
+            <View
+                style={{
+                    position: 'absolute',
+                    height: '100%',
+                    width: '100%',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    overflow: 'visible',
+                }}
+            >
+                <View
+                    style={{
+                        position: 'absolute',
+                        left: 0,
+                        height: '100%',
+                        justifyContent: 'center',
+                        paddingHorizontal: 6,
+                    }}
+                >
+                    <View style={{backgroundColor: Color(colors.secondaryContainer).fade(.2).rgb().toString(), borderRadius: 12, paddingHorizontal: 8}}>
+                        <Text selectable={false} style={[style.text]}>
+                            {t`Lvl.`} {levelInfo.level}
+                        </Text>
+                    </View>
+                </View>
+                <View style={{backgroundColor: Color(colors.secondaryContainer).fade(.2).rgb().toString(), borderRadius: 12, paddingHorizontal: 8}}>
+                    <Text selectable={false} style={[style.text]}>
+                        {levelInfo.xp} / {levelInfo.levelXpCap}
+                    </Text>
+                </View>
+            </View>
+        </View>
+    );
 };
