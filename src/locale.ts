f472951f4077385f9acaf35c1c7917902674a3e5
Smashinfries <jus10luka@gmail.com>
2026-09-22 16:28:17 -0500

    feat: add more languages and translations

--- src/locale.ts
+++ src/locale.ts
@@ -1,11 +1,17 @@
 import { i18n } from "@lingui/core";
 
-/**
- * We do a dynamic import of just the catalog that we need
- * @param locale any locale string
- */
+const loaders: Record<string, () => Promise<any>> = {
+    de: () => import('../locales/de/messages'),
+    en: () => import('../locales/en/messages'),
+    es: () => import('../locales/es/messages'),
+    fr: () => import('../locales/fr/messages'),
+    ja: () => import('../locales/ja/messages'),
+    ko: () => import('../locales/ko/messages'),
+    zh: () => import('../locales/zh/messages')
+};
+
 export const dynamicActivate = async (locale: string) => {
-  const { messages } = await import(`./locales/${locale}/messages`);
-  i18n.load(locale, messages);
-  i18n.activate(locale);
+    const { messages } = await (loaders[locale] ?? loaders['en'])();
+    i18n.load(locale, messages);
+    i18n.activate(locale);
 }
