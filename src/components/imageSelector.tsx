bb2c66592d02e01565f50f3c2f8ca922cb90d806
Smashinfries <jus10luka@gmail.com>
2026-09-22 16:13:28 -0500

    feat: enhance ImageSelector with improved image handling and loading indicators

--- src/components/imageSelector.tsx
+++ src/components/imageSelector.tsx
@@ -1,5 +1,5 @@
-import { View, Pressable, useWindowDimensions } from 'react-native';
-import { ActivityIndicator, Button, Chip, Text } from 'react-native-paper';
+import { View, Pressable, useWindowDimensions, Platform } from 'react-native';
+import { Button, Chip, Text } from 'react-native-paper';
 import * as ImagePicker from 'expo-image-picker';
 import { Image } from 'expo-image';
 import { BlurView } from 'expo-blur';
@@ -7,6 +7,10 @@ import Animated, { FadeIn, FadeOut } from 'react-native-reanimated';
 import { RankInfo } from '../types';
 import { useStatsStore } from '../store/stats';
 import { useLingui } from '@lingui/react/macro';
+import { useImageStore } from '../store/image';
+import { useDragDrop } from '../hooks/useDragDrop';
+import { useAppTheme } from '../providers/theme';
+import { CircularProgress } from './common/progressBar';
 
 const RankChip = ({ rank }: { rank: RankInfo }) => {
 	return (
@@ -22,16 +26,21 @@ type ImageSelectorProps = {
 	onImagePick: () => void;
 	isLoading?: boolean;
 };
-const ImageSelector = ({ image, rank, isLoading, onImagePick }: ImageSelectorProps) => {
+const ImageSelector = ({ rank, isLoading, onImagePick }: ImageSelectorProps) => {
 	const { height, width } = useWindowDimensions();
-	const { isEnabled } = useStatsStore();
+    const imageUri = useImageStore(state => state.image?.uri);
+	const isEnabled = useStatsStore(state => state.isEnabled);
+    const { colors } = useAppTheme();
+    const { isHovered } = useDragDrop();
     const { t } = useLingui();
 
 	return (
 		<Pressable
+            nativeID='drop-zone'
 			onPress={onImagePick}
 			style={{
 				height: height / 3 + 16,
+                minHeight: Platform.OS === 'web' ? 400 : undefined,
 				alignSelf: 'center',
 				width: '100%',
 				borderStyle: 'solid',
@@ -39,25 +48,35 @@ const ImageSelector = ({ image, rank, isLoading, onImagePick }: ImageSelectorPro
 				justifyContent: 'center',
 				flexDirection: 'row',
 				overflow: 'hidden',
-				// @ts-expect-error: Wails specific
-				'--wails-drop-target': 'drop',
 			}}
 		>
-			{image ? (
-				<View style={{ flex: 1 }}>
-					<Image
-						source={{ uri: image.uri }}
+			{imageUri ? (
+				<View style={{ width: '100%', height: '100%' }}>
+                    <Image 
+                        source={{ uri: imageUri }}
 						style={{
 							width: '100%',
-							height: height / 3,
-							paddingVertical: 8,
+							height: '100%',
+                            position: 'absolute',
+							// paddingVertical: 8,
 						}}
-						contentFit="contain"
-					/>
+						contentFit="cover"
+                        blurRadius={18}
+                    />
+                    <View style={{height: '100%', width: '100%', paddingTop: 80, paddingBottom: 8,}}>
+                        <Image
+                            source={{ uri: imageUri }}
+                            style={{
+                                width: '100%',
+                                height: '100%',
+                            }}
+                            contentFit="contain"
+                        />
+                    </View>
 				</View>
 			) : (
 				<>
-					<Button mode="text" icon={'upload'} onPress={onImagePick}>
+					<Button mode="text" icon={'upload'} onPress={onImagePick} style={{marginTop: 80}}>
 						{t`Upload Image`}
 					</Button>
 				</>
@@ -74,21 +93,25 @@ const ImageSelector = ({ image, rank, isLoading, onImagePick }: ImageSelectorPro
 							alignItems: 'center',
 							justifyContent: 'center',
 							backgroundColor: 'transparent',
+                            paddingTop: 82
 						},
 					]}
 				>
-					<ActivityIndicator />
+					<CircularProgress hosted={Platform.OS !== 'web'} value={0} indeterminate matchContents />
 				</BlurView>
 			)}
 			{rank && isEnabled && (
 				<Animated.View
 					entering={FadeIn}
 					exiting={FadeOut}
-					style={{ position: 'absolute', bottom: 5, right: 5 }}
+					style={{ position: 'absolute', bottom: 10, right: Platform.OS === 'web' ? 24 : 10 }}
 				>
 					<RankChip rank={rank} />
 				</Animated.View>
 			)}
+            {isHovered && <Animated.View entering={FadeIn} exiting={FadeOut} style={{ position: 'absolute', width: '100%', height: '100%'}}>
+                <View style={{backgroundColor: colors.surfaceContainerHigh, opacity: 0.75, width: '100%', height: '100%'}} />
+                </Animated.View>}
 		</Pressable>
 	);
 };
