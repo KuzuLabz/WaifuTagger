99c697fd70d596753ca61a85d5e4a98901194025
Smashinfries <jus10luka@gmail.com>
2026-09-22 22:43:25 -0500

    feat: temp remove inline module

--- src/utils/memory/index.native.ts
+++ src/utils/memory/index.native.ts
@@ -4,25 +4,28 @@ import { requireNativeModule } from "expo";
 import { Directory } from "expo-file-system";
 import { useModelsStore } from "../../store/models";
 
-type SystemMemoryInfo = {
-    availableMemory: number;
-    usedMemory: number;
-};
-interface DeviceMemoryModuleInterface {
-    getSystemMemoryInfo: () => SystemMemoryInfo;
-}
-const DeviceMemoryModule = requireNativeModule<DeviceMemoryModuleInterface>('DeviceMemoryModule');
+// type SystemMemoryInfo = {
+//     availableMemory: number;
+//     usedMemory: number;
+// };
+// interface DeviceMemoryModuleInterface {
+//     getSystemMemoryInfo: () => SystemMemoryInfo;
+// }
+// const DeviceMemoryModule = requireNativeModule<DeviceMemoryModuleInterface>('DeviceMemoryModule');
 
 export const getMemoryInfo = async (): Promise<MemoryInfo | null> => {
-    const systemMemory = DeviceMemoryModule.getSystemMemoryInfo();
-    return {
-        ram: {
-            appUsed: DeviceInfoModule.getUsedMemory(),
-            free: systemMemory.availableMemory,
-            total: DeviceInfoModule.totalMemory,
-            used: systemMemory.usedMemory
-        }
-    };
+    // Unable to get inline module working!
+    // const systemMemory = DeviceMemoryModule.getSystemMemoryInfo();
+    // return {
+    //     ram: {
+    //         appUsed: DeviceInfoModule.getUsedMemory(),
+    //         free: systemMemory.availableMemory,
+    //         total: DeviceInfoModule.totalMemory,
+    //         used: systemMemory.usedMemory
+    //     }
+    // };
+
+    return null;
 };
 
 export const getStorageInfo = async (): Promise<StorageInfo | null> => {
