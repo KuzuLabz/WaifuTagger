176f36d3d083d52d781cf33c9eb76ff50ff795f9
Smashinfries <jus10luka@gmail.com>
2026-09-23 21:58:12 -0500

    Add Chinese (Simplified and Traditional) localization files and remove old Chinese localization

--- lingui.config.js
+++ lingui.config.js
@@ -2,7 +2,12 @@ import { defineConfig } from "@lingui/cli";
 
 export default defineConfig({
     sourceLocale: "en",
-    locales: ["en", "es", "fr", "de", "zh", "ja", "ko"],
+    locales: ["en", "es", "fr", "de", "zh-CN", "zh-TW", "ja", "ko"],
+    fallbackLocales: {
+        "zh-CN": 'zh',
+        "zh-HK": "zh-TW",
+        default: 'en'
+    },
     catalogs: [
         {
             path: "./locales/{locale}/messages",
